10396c06ed3757db6ee711d724e8f283633fe3e3
Alyssa Pratt <snubly1@gmail.com>
2016-07-02 20:06:07 -0400

    changed some css and html

--- app/assets/javascripts/votingbuttons.js
+++ app/assets/javascripts/votingbuttons.js
@@ -1,29 +1,17 @@
 $(document).ready(function() {
-  //with this next bit I am trying to get it to pick up the previous upvote and downvote sizes so that
-  //I can add/subtrach the data.message from it. The problem is that I am not writting it correctly
-  //and it does not know that I am talking about a specifc post so it finds the nums for all of them
-  //and jams them together and then does the calculations. Any ideas on how I can format this so that
-  //it will work?
-  var upvoteSize = parseInt($(".voteButtons").find(".upvotessize").text())
-  var downvoteSize = parseInt($(".voteButtons").find(".downvotessize").text())
-  console.log(upvoteSize)
-  console.log(downvoteSize)
-  var score = (upvoteSize - downvoteSize)
-
 
   $(".upvote").click(function() {
     var findPostId = $(this).find("p.id-of-post").text();
     var upButton = $(this).find(".buttonUp");
     var downButton = $(this).find(".buttonDown");
-    
+
     upButton.addClass("orange")
     $(".voteButtons").find(".buttonDown").removeClass("orange")
 
     $.ajax("/post/" + findPostId + "/upvote.json", {
       method: "PUT",
       success: function(data) {
-        //This does it for all of them and I need it to only do it for the one I am hitting the vote for
-        $(".total-votes-size").text(data.message)
+        console.log(data.message)
       },
       error: function() { alert("Something went wrong!") }
     });
@@ -43,8 +31,8 @@ $(document).ready(function() {
     $.ajax("/post/" + findPostId + "/downvote.json", {
       method: "PUT",
       success: function(data) {
-        $(".total-votes-size").text(data.message)
-      },
+        console.log(data.message)
+     },
       error: function() { alert("Something went wrong!") }
     });
   });
